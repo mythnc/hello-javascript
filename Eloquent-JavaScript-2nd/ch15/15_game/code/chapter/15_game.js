3b3ebdc70123ce3147a5ce91ff57526603180793
N.C Wu <mythnc@gmail.com>
2018-01-25 11:40:52 +0800

    Add game over feature

--- Eloquent-JavaScript-2nd/ch15/15_game/code/chapter/15_game.js
+++ Eloquent-JavaScript-2nd/ch15/15_game/code/chapter/15_game.js
@@ -344,15 +344,21 @@ function runLevel(level, Display, andThen) {
 }
 
 function runGame(plans, Display) {
-  function startLevel(n) {
+  function startLevel(n, lives) {
     runLevel(new Level(plans[n]), Display, function(status) {
-      if (status == "lost")
-        startLevel(n);
+      if (status == "lost") {
+	    if (lives == 0) {
+		 startLevel(0, 3)
+		}
+		else {
+		  startLevel(n, lives - 1);
+		}
+	  }
       else if (n < plans.length - 1)
-        startLevel(n + 1);
+        startLevel(n + 1, lives);
       else
         console.log("You win!");
     });
   }
-  startLevel(0);
+  startLevel(0, 3);
 }
